53166afa6e3403269973d923328e3eb678e48a1c
wesleymarin <wesley.marin@ucsf.edu>
2016-07-27 16:03:08 -0700

    Trying to push

--- PING_gc_caller.R
+++ PING_gc_caller.R
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PING.  If not, see <http://www.gnu.org/licenses/>.
-
+#
 ping_gc <- function(
   run.MIRA = TRUE, 
   run.KFF = TRUE, 
