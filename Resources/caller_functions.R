6fab41b9de4a97cdf145714a76ea760f5751c59c
RaviDandekar <ravi.dandekar@gmail.com>
2019-03-11 12:03:18 -0700

    Added function to filter reads from 2DL1 SAM files that are contaminating position 1279 (CDS position 13)

--- Resources/caller_functions.R
+++ Resources/caller_functions.R
@@ -1549,6 +1549,57 @@ filter_contam_snps <- function(x,current.locus,KIR_sample_snps, vcf.location.gen
 }
 
 
+filter_contam_reads_from_sam_file <- function(sam_file, sequence){
+  # Custom probes to identify contaminating read pairs responsible for incorrect base at CDS position 13
+  probe1 <- "CACCGGCAGCACCATGTCGCTCACGGT"
+  probe2 <- "CACAGACAGCACCATGTCGCTCATGGTC"
+  probe3 <- "TCCGGCACCACCATGTCGCTCATGGTCA"
+  probe4 <- "CACCGGCAGCACCATGTTGCTCATGGTC"
+  probe5 <- "CACCGGCAGCACCATGTCGCTCATGGTC"
+  probe6 <- "CACCGGCAGCACCATGTCGCTCACGGTC"
+  probe7 <- "CACGGTCGTCAGCGTGGC"
+  
+  # Create data table of SAM file
+  sam.df <- read.table(sam_file, skip = 3, col.names = 1:20,header = F, check.names=F, fill=T,sep = "\t", stringsAsFactors = F)
+  
+  output.samTable <- as.data.table(sam.df)
+  colnames(output.samTable)[1]  <- 'read_name'
+  colnames(output.samTable)[3]  <- 'reference_name'
+  colnames(output.samTable)[4]  <- 'ref_pos'
+  colnames(output.samTable)[10] <- 'read_seq'
+  
+  output.samTable.subset <- output.samTable[grepl(probe1,output.samTable$read_seq)  |
+                                              grepl(probe2,output.samTable$read_seq)|
+                                              grepl(probe3,output.samTable$read_seq)|
+                                              grepl(probe4,output.samTable$read_seq)|
+                                              grepl(probe5,output.samTable$read_seq)|
+                                              grepl(probe6,output.samTable$read_seq)|
+                                              grepl(probe7,output.samTable$read_seq),]
+  
+  contam_read_ids <- output.samTable.subset$read_name
+  
+  # Filter contaminating read pairs out of SAM file
+  new_sam_file <- paste0(sequence,"_2DL1.filt.sam")
+  sink(new_sam_file)
+  con = file(sam_file, "r")
+  while (TRUE){
+    line = readLines(con, n=1)
+    if (length(line) == 0){
+      break
+    }
+    read.id <- unlist(strsplit(line,"\t"))[1]
+    if (! read.id %in% contam_read_ids){cat(paste0(line, "\n"))}
+  } 
+  
+  close(con)
+  sink()
+  
+  # Replace SAM file with filtered SAM file
+  file.remove(paste0(sequence,"_2DL1.sam"))
+  system2("mv", c(new_sam_file,sam_file))
+}
+
+
 ##==================================================================
 ## PING Allele Caller Functions ------------------------------------
 ##==================================================================
